f711c7e33fec4997fb0a41145e7a12d1a3b58173
kan51 <kandai.oiso@gmail.com>
2025-09-29 20:45:23 +0900

    commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # from langchain.chat_models import ChatOpenAI
 # llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.5)
+llm = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 
 import streamlit as st
