70ad96df8943783a3788f133e5a76317f0ad827b
kan51 <kandai.oiso@gmail.com>
2025-10-01 20:40:27 +0900

    commit

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ load_dotenv()
 # from langchain.chat_models import ChatOpenAI
 # llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.5)
 from openai import OpenAI
-llm = OpenAI
+llm = OpenAI(
+    model="gpt-4o-mini",
+    temperature=0.5
+)
 
 
 st.title("サンプルアプリ: 専門家アドバイザーアプリ")
